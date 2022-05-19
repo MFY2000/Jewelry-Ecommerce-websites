5c1db7e3d3cb4c8e79d80117da197ddc5f7b567e
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-19 20:05:58 +0500

    MFY auto commit at 19/05/2022 20:05:14

---
+++ website/php/Components/terms.php
@@ -0,0 +1,30 @@
+<?php 
+
+  function getTMC()
+  {
+    return "
+    <section class='termsCondionPage'>
+    <div class='container'>
+        <div class='row'>
+            <div class='col-md-12'>
+                <div class='tmc text-center'>
+                    <h1 class='secondaryHeading p'>Terms & Conditions</h1>
+                    <p class='primaryParagraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis facere error tenetur praesentium, odio facilis dolores et eos quisquam aliquid odit dolorem, velit voluptas eaque. Et deserunt totam voluptate enim nobis sit earum
+                        aliquid officiis ipsam ipsa temporibus a assumenda sed minima nulla, cumque accusantium illo ratione quis, voluptatibus unde deleniti. Et nulla voluptates facere earum, nemo quod odit repudiandae quas dolores laboriosam error
+                        molestiae provident dicta commodi unde minus sunt tempore ipsum ex necessitatibus. Eveniet nihil nobis sunt nam soluta iusto iure, nostrum corporis saepe ipsum architecto ut ducimus rem harum dolorem sed? Nisi voluptatem exercitationem
+                        aspernatur vero quo commodi ducimus doloribus, nobis accusamus harum vel! Ad temporibus laborum, a eaque maiores incidunt praesentium inventore fuga blanditiis amet, placeat recusandae at delectus fugiat est officiis illum
+                        quam? Cum, minima. Eaque natus perspiciatis aliquam tenetur laboriosam omnis assumenda nulla modi sapiente. Quas optio consectetur earum nisi voluptate aspernatur eaque sed, debitis recusandae nihil dicta quibusdam impedit
+                        provident sunt id? Molestiae accusantium porro itaque ullam fugiat voluptatem sed expedita soluta nihil id ea enim blanditiis, nobis fugit molestias dignissimos maiores possimus. Officiis aliquam deserunt aliquid facilis id
+                        tempora, iusto non consectetur voluptatum facere delectus nostrum eius, accusamus repellat omnis ad illo praesentium. Laborum sint dicta corporis voluptate harum, molestias ratione aperiam ad dolorem consequatur sit quos quibusdam
+                        iure nobis qui consectetur amet tempora laboriosam cupiditate minima, maiores corrupti perferendis voluptatibus? Necessitatibus harum in at nisi, vel temporibus quae laborum nostrum excepturi dicta impedit, ut similique, iusto
+                        consequatur delectus dolor tenetur corporis.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>";
+  }
+
+?>
+
+
