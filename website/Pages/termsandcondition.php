7b2c7605c36ce5872acf5bf843e3003b31e5dfc8
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-19 20:05:42 +0500

    MFY auto commit at 19/05/2022 20:05:14

---
+++ website/Pages/termsandcondition.php
@@ -0,0 +1,13 @@
+<?php 
+    
+    include '..\php\MainBody.php'; 
+
+    $index = new PageBody("termsandcondition", "..");
+    $index->navBar();
+    $index->banner();
+    $index->termSection();
+    $index->footer();
+    
+    echo $index->body();
+    
+?>
