92572eca986bbd1bca2564e1ed64ae42d231e543
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-19 20:05:57 +0500

    MFY auto commit at 19/05/2022 20:05:14

---
+++ website/php/Components/newsLetter.php
@@ -0,0 +1,32 @@
+<?php
+  
+  function newsLetterSection($root){
+  return "<!-- contact start -->
+  <section class='contact'>
+      <div class='container'>
+          <div class='row'>
+              <div class='col-md-12'>
+                  <div class='contactHeader'>
+                      <h1 class='secondaryHeading'>Subscribe Our news Letter</h1>
+                      <p class='primaryParagraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                  </div>
+              </div>
+          </div>
+          <div class='row'>
+              <div class='col-md-12'>
+                  <div class='contactfeilds'>
+                      <form>
+                          <input class='contactInput' type='email' placeholder='Please enter your email address'>
+                          <input type='submit' class='btnStyle' value='Subscribe'>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+  <!-- contact end -->";
+  }
+
+?>
+
+    
