b71167ae47a0bd32d100e604d1d7750bc9c8c4fb
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-19 20:05:59 +0500

    MFY auto commit at 19/05/2022 20:05:14

---
+++ website/php/Services/Form/signupForm.php
@@ -0,0 +1,33 @@
+<?php
+  include '..\Query\InsertQuery.php'; 
+  include '..\Query\GetQuery.php'; 
+  
+  $message = "";
+  $error = true; 
+
+
+  
+  // if(!isset($_POST["email"])){
+  //   $message = "Name is required";
+  // }else if(!isset($_POST["password"])) {
+  //   $message = "phone number is required";
+  // }else{
+    $query = $insertQuery_signUp.("( '".$_POST["Email"]."', '".$_POST["Password"]."', '".$_POST["Firstname"]."', '".$_POST["Lastname"]."', '1' )");
+
+    $result = $conn->query($query);
+    
+    if($result){
+      getUser($_POST["Email"], $_POST["Password"]);
+      $message = "Successfully login";
+      $error = false;
+    }else{
+      $message = "Error".$conn->error;
+    }
+  // }
+  session_start();
+
+
+  $_SESSION["Login"]["message"] = $message;
+  $_SESSION["Login"]["error"] = $message;
+  header("Location: ..\..\..\Pages\index.php");
+?>
