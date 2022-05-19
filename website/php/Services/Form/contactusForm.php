a447b633c173174d5022bb2eae7a6789644f3171
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-19 20:05:59 +0500

    MFY auto commit at 19/05/2022 20:05:14

---
+++ website/php/Services/Form/contactusForm.php
@@ -0,0 +1,33 @@
+<?php
+  include '..\DBConnection.php'; 
+  include '..\Query\InsertQuery.php'; 
+  
+  $message = "";
+  $error = true; 
+
+  if(!isset($_POST["name"])){
+    $message = "Name is required";
+  }else if(!isset($_POST["phone"])) {
+    $message = "phone number is required";
+  }else if(!isset($_POST["address"])) {
+    $message = "Address is required";
+  }else if(!isset($_POST["message"])) {
+    $message = "message is required";
+  }else{
+    $query = $insertQuery_contactUs.("('".$_POST["name"]."','".$_POST["phone"]."','".$_POST["address"]."','".$_POST["message"]."')");
+    
+    $result = $conn->query($query);
+    
+    if($result){
+      $message = "Successfully Submit";
+      $error = false;
+    }else{
+      $message = "Error".$conn->error;
+    }
+  }
+  session_start();
+
+  $_SESSION["Contact"]["message"] = $message;
+  $_SESSION["Contact"]["error"] = $message;
+  header("Location: ..\..\..\Pages\contactus.php");
+?>
