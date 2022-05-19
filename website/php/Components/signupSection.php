d2a0ccdec51ccbe4f1674905b61ea73eda7b7bd1
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-19 20:05:57 +0500

    MFY auto commit at 19/05/2022 20:05:14

---
+++ website/php/Components/signupSection.php
@@ -0,0 +1,37 @@
+<?php 
+
+  function getSignUpSection(){
+
+    return "<section class='authPage'>
+    <div class='container'>
+        <div class='row'>
+            <div class='col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-12'>
+                <div class='auth'>
+                    <form method='post' action='..\php\Services\Form/signupForm.php'>
+                        <h1>Signup</h1>
+                        
+                        <input class='contactInput' type='email' name='Email' placeholder='Email Address'><br>
+                        <input class='contactInput' type='text' name='Password' placeholder='Password'><br>
+                        <input class='contactInput' type='text' name='RePassword' placeholder='Re type Password'><br>
+                        <input class='contactInput' type='text' name='Firstname' placeholder='Firstname'><br>
+                        <input class='contactInput' type='text' name='Lastname' placeholder='Lastname'><br>
+                        
+                        <div class='cbi'>
+                            <input type='checkbox' name='registering'>&nbsp;
+                            <p>By registering you agree our <b><a href='termsandcondition.php'>Terms of user</a></b></p>
+                        </div>
+                        <input type='submit' class='btnStyle' value='Signup'>
+                        <br>
+                        <br>
+                        <a href='login.php?page=signIN'>Already have a account ?</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>";
+
+  }
+
+
+?>
