8ca8ecd9d4f79996b27e7cbfee75a038857ad48e
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-19 20:05:55 +0500

    MFY auto commit at 19/05/2022 20:05:14

---
+++ website/php/Components/Banner.php
@@ -0,0 +1,21 @@
+<?php 
+
+  function BannerSection($name){
+    return"    <!-- banner start -->
+    <section class='$name theme-banner'>
+        <div class='container'>
+            <!-- col-sm-6 col-sm-offset-2 col-xs-6 col-xs-offset-1 -->
+            <div class='row mini_banner_row'>
+                <div class='col-md-6 col-md-offset-3'>
+                    <div class='mini_banner_center text-center'>
+                        <h1>$name</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- banner end -->";
+  }
+
+
+?>
