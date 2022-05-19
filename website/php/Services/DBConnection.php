8dd42a1faea28972a292fb514071ca7a2a7741b1
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-19 20:05:59 +0500

    MFY auto commit at 19/05/2022 20:05:14

---
+++ website/php/Services/DBConnection.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$databasename = 'jewelry';
+
+$conn = mysqli_connect($servername,$username,$password,$databasename); 
+if (!$conn) {
+    
+}else{
+    // echo ;
+}
+
+
+
+?>
