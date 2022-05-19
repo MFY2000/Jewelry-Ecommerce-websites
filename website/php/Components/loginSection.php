b308f8da9e0c4424e544441aa0f5b0b9e0c868bf
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-19 20:05:57 +0500

    MFY auto commit at 19/05/2022 20:05:14

---
+++ website/php/Components/loginSection.php
@@ -0,0 +1,25 @@
+<?php
+  
+  function getLoginSection(){
+  return "<section class='authPage'>
+  <div class='container'>
+      <div class='row'>
+          <div class='col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-12'>
+              <div class='auth'>
+                  <form method='post' action=''>
+                      <h1>Login</h1>
+                      <input class='contactInput' name='email' type='email' placeholder='Email Address'><br>
+                      <input class='contactInput' name='password' type='password' placeholder='Password'><br>
+                      <input type='submit' class='btnStyle' value='Login'>
+                      <br>
+                      <br>
+                      <a href='login.php?page=signUP'>Create new Account</a>&nbsp;&nbsp;&nbsp;<a href=''>Forget passowrd ?</a>
+                  </form>
+              </div>
+          </div>
+      </div>
+  </div>
+</section>";
+  }
+
+?>
