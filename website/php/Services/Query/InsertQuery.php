69c8a7c122cbe8c6b594b9b7cadd2c48d48b7b92
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-19 20:06:00 +0500

    MFY auto commit at 19/05/2022 20:05:14

---
+++ website/php/Services/Query/InsertQuery.php
@@ -0,0 +1,11 @@
+<?php
+  $insertQuery_contactUs = "INSERT INTO `contact_us`( `name`, `phoneNumber`, `address`, `message`) VALUES ";
+  $insertQuery_signUp = "INSERT INTO `user`(`Email`, `Password`, `Firstname`, `Lastname`, `Status`) VALUES ";
+
+
+
+
+
+
+
+?>
