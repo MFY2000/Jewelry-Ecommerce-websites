f14d3479630a99a196222bca7f4d4f4fb9961bd6
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-23 09:33:07 +0500

    MFY auto commit at 23/05/2022 09:32:02

--- website/php/Components/contactus.php
+++ website/php/Components/contactus.php
@@ -14,9 +14,7 @@
                     </div>
                     <div class='row'>
                         <div class='col-md-12'>
-                            <div class='maincontactfeilds'>
-                                
-                                
+                            <div class='maincontactfeilds'>    
                                 <form method='post' action='..\php\Services\Form\contactusForm.php'>
                                     <input name='name' class='contactInput' type='text' placeholder='Please enter your name'><br>
                                     <div class='minicontactfeilds'>
@@ -26,14 +24,7 @@
                                     <textarea class='contactInput' placeholder='Please enter your Message' name='message' rows='3'></textarea><br>
                                     <input type='submit' class='btnStyle' value='Submit'>
                                 </form>
-                                <div style='margin: 30px 0 0 18px' id='Contact_message'>
-                                ".(isset($_SESSION["Contact"]["message"]) && $_SESSION["Contact"]["error"] ? $_SESSION["Contact"]["error"] : $_SESSION["Contact"]["message"])."
-                                <script>
-                                    if(".$_SESSION["Contact"]["error"] && isset($_SESSION["Contact"]["message"])."){
-                                        document.getElementById('Contact_message').value = '';
-                                    }
-                                </script>
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
