651601f833297c83bca96434d30095697dbead94
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-23 09:33:11 +0500

    MFY auto commit at 23/05/2022 09:32:02

--- website/php/Services/Query/GetQuery.php
+++ website/php/Services/Query/GetQuery.php
@@ -1,16 +1,18 @@
 <?php
+  include("../DBConnection.php");
+
   $getQuery = "select `uid` from `user` where  ";
 
 
-
   function getUser($email, $password){
-    $query = $GLOBALS["getQuery"]."`Email` = '".$_POST["Email"]."', `Password` ='".$_POST["Password"]."' ";
-    $result = $conn->query($query);
+    $query = $GLOBALS["getQuery"]."`Email` = '".$email."', `Password` ='".$password."' ";
+    $result = $GLOBALS["conn"]->query($query);
 
     if ($result->num_rows > 0) {
-      while($row = $result->fetch_assoc()) {
+      while($row = $result -> fetch_row()) {
         $_SESSION["Login"]["id"] = $row["uid"];
       }
     }
+    return $result;
   }
 ?>
