ce750ded3cd5dae5271f9b3667ca510dae4cb6fa
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-23 09:33:04 +0500

    MFY auto commit at 23/05/2022 09:32:02

--- website/php/Components/Aboutus.php
+++ website/php/Components/Aboutus.php
@@ -13,13 +13,13 @@
                           <p class='primaryParagraph'>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis
                               unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                           </p>
-                          <a href='' class='btnStyle'>Learn More</a>
+                          <a href='$root/Pages/aboutus.php' class='btnStyle'>Learn More</a>
                       </div>
                   </div>
                   <div class='col-md-6'>
                       <div class='about_side_2'>
-                          <img src='$root/images/aboutPic1.png' alt='about pic 1'>
-                          <img src='$root/images/aboutPic2.png' alt='about pic 2'>
+                          <img src='$root/images/Product/aboutPic1.png' alt='about pic 1'>
+                          <img src='$root/images/Product/aboutPic2.png' alt='about pic 2'>
                       </div>
                   </div>
               </div>
