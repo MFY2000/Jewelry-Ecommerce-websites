586c372103beac96f6be70d04d9eefde97e563ee
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-24 07:42:32 +0500

    MFY auto commit at 24/05/2022 07:40:11

--- website/Admin/productform.php
+++ website/Admin/productform.php
@@ -1,17 +1,23 @@
 <?php
+include ("../php/services/DBConnection.php");
+
+
 $target_dir = "../images/Product/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-if (! is_dir('uploads') ) mkdir ( 'uploads' , 0755);
+if (! is_dir($target_dir) ) mkdir ( $target_dir , 0755);
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
+    uploadData();
+    
+    
+    
   } else {
     echo "File is not an image.";
     $uploadOk = 0;
@@ -43,9 +49,40 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    uploadData();
+    
     echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
 }
+
+function uploadData(){
+  $Query = "INSERT INTO `product`(`Title`, `Price`, `Details`, `Orders`) VALUES ('".$_POST['title']."','".$_POST['price']."','".$_POST['details']."','1')";
+  
+
+  $result = $GLOBALS["conn"]->query($Query);
+  if($result){
+    echo "Successfully Uploaded";
+
+    $result = $GLOBALS["conn"]->query("SELECT `PID` FROM `product` order by `PID` desc limit 1");
+    $row = $result->fetch_assoc();
+
+
+    $Query = "INSERT INTO `product_images`(`PID`, `imageName`) VALUES ('".$row['PID']."','".basename($_FILES["fileToUpload"]["name"])."')";
+
+    $result = $GLOBALS["conn"]->query($Query);
+    if($result){
+      echo "Successfully Uploaded";
+    }
+  }else{
+
+  }
+
+  echo "<script>
+  history.go();
+  </script>";
+}
+
+
 ?>
