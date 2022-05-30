bd28f0901dd31002d61809ca1bd94693d01f43f5
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-05-30 18:02:44 +0500

    MFY auto commit at 30/05/2022 17:03:10

---
+++ website/php/Services/Query/Query.php
@@ -0,0 +1,37 @@
+<?php 
+  class Query{
+    
+    public $Getquery;
+    public $insertquerys;
+    
+    function __construct(){
+      $this->Getquery = new getQuery();
+      $this->insertquerys = new InsertQuery();
+    }
+    
+    // function getData($query){
+    //   $this->result = $this->querys->getData($this->query);
+    //   return $this->result;
+    // }
+   
+    }
+  class getQuery{
+    public $getQuery_product = "select `PID`,`Title` from `product` limit ";
+    public $getQuery = "select `uid` from `user` where  ";
+    public $getQuery_categories = "SELECT `categories`.`CID`, `categories`.`Title`, `product_images`.`imageName` FROM `categories` INNER JOIN `product` ON `product`.CID = `categories`.CID INNER JOIN `product_images` ON `product`.PID = `product_images`.PID";
+    public $getQuery_Product = "SELECT `product`.`PID`,`product`.`Title`,`product_images`.`imageName` FROM `product` INNER JOIN `product_images` ON `product`.PID = `product_images`.PID";
+    public $getQuery_Single_Product = "SELECT * FROM `categories` INNER JOIN `product` ON `product`.CID = `categories`.CID INNER JOIN `product_images` ON `product`.PID = `product_images`.PID";
+
+  }  
+
+  class InsertQuery{
+    public $insertQuery_contactUs = "INSERT INTO `contact_us`( `name`, `phoneNumber`, `address`, `message`) VALUES ";
+    public $insertQuery_signUp = "INSERT INTO `user`(`Email`, `Password`, `Firstname`, `Lastname`, `Status`) VALUES ";  
+    
+  }  
+
+
+  // }
+
+
+?>
