4e97970485515211cac7de763eb2bd5f44c37b74
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-06-24 11:18:39 +0500

    MFY auto commit at 24/06/2022 11:18:19

--- assets/php/Components/snackBar.php
+++ assets/php/Components/snackBar.php
@@ -1,31 +1,30 @@
 <?php 
 
 function getSnackBar($message, $type){
-  return "
-  <div id='snackbar' class='$type'>
-    <span>
-      ".($type == 'error' ? "<i class='fa fa-exclamation'></i>":"<i class='fa fa-check-square'></i>")."
+  $messageBody = "";
+  
+    if($type != "Success"){
+      $messageBody = '<div class="wtg-alert wtg-alert-danger alertBox" id="alert">
+      <div class="wtg-alert-title">
+        <i class="wtg-alert-icon glyphicon glyphicon-star"></i>
+        <strong>Error</strong>
+      </div>
+      <p class="wtg-alert-text">
+        '.$message.'
+      </p>
+    </div>';
+    }else{
+      $messageBody = '<div class="wtg-alert wtg-alert-success alertBox " id="alert">
+      <div class="wtg-alert-title">
+        <i class="wtg-alert-icon glyphicon glyphicon-star"></i>
+        <strong>Success</strong>
+        </div>
+        <p class="wtg-alert-text">
+        '.$message.'
+        </p>';
+      }
       
-      $message..
-    </span>
-  
-    <span class='btn-close'><i class='fa fa-window-close' onclick='closeWindow()'></i><span>
-  </div>
-
-  <script>
-  var x = document.getElementById('snackbar');
-  
-    function closeWindow() {
-      x.className = '';
-    }
-  
-    setTimeout(function(){
-      x.className += ' show';
-    } , 1000);
-    
-    setTimeout(closeWindow , 5000);
-  </script>
-  ";
+      return  $messageBody. "<script>setTimeout(function(){document.getElementById('alert').style.display = 'none';},5000);</script>";
 }
 
 
