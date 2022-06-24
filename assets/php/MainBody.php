c56b7bb3f0cf624df804fae9b9f717a07c390d7b
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-06-24 11:18:39 +0500

    MFY auto commit at 24/06/2022 11:18:19

--- assets/php/MainBody.php
+++ assets/php/MainBody.php
@@ -1,6 +1,6 @@
 <?php 
 
-$import_Module =  ["loginSection" ,"NavBar" ,"Slider" ,"banner" ,"aboutus" ,"Product" ,"Categories" ,"newsLetter" ,"Footer" ,"contactus" ,"signupSection" ,"snackBar" ,"terms" ,"DetailsSection", "cartSection", "checkOutSection"];
+$import_Module =  ["loginSection" ,"NavBar" ,"Slider" ,"banner" ,"aboutus" ,"Product" ,"Categories" ,"newsLetter" ,"Footer" ,"contactus" ,"signupSection" ,"snackBar" ,"terms" ,"DetailsSection", "cartSection", "checkOutSection", "CheckoutTotalSection"];
 
 session_start();
 
@@ -9,7 +9,8 @@ class PageBody {
   public $title;
   public $root;
   public $template;
-  
+  public $db_instant;
+
   function __construct($title, $path){
     $this->title = $title;
     $this->root = $path;
@@ -27,7 +28,20 @@ class PageBody {
   }
   
 
+  function isSession(){
+    if(isset($_SESSION['user'])){
+      return true;
+    }else{
+      return false;
+    }
+
+
+
+  }
+
   function body() {
+    $this->alertSection();
+
     return "<!DOCTYPE html>
     <html lang='en'>
     
@@ -37,23 +51,32 @@ class PageBody {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>Jewellery design (".$this->title.")</title>
         <link rel='stylesheet' type='text/css' href='$this->root/assets/css/style.css' />
+        <link rel='stylesheet' type='text/css' href='$this->root/assets/css/alertCss.css' />
         <link rel='stylesheet' type='text/css' href='$this->root/assets/bootstrap/css/bootstrap.min.css' />
         
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css' />
         
-        <script src='$this->root/bootstrap/js/bootstrap.js'></script>
-        <script src='$this->root/bootstrap/jQuery/jQuery.js'></script>
-    </head>
+        <script src='$this->root/assets/bootstrap/js/bootstrap.js'></script>
+        <script src='$this->root/assets/bootstrap/jQuery/jQuery.js'></script>
+        <script src='$this->root/assets/js/index.js'></script>
+        
+        <script>
+        function moveTOCheckout(){ 
+          var total = document.getElementById('CartTotal').innerHTML;
+          window.location.href = `".$this->root."/assets/php/Services/Form/CheckoutFunction.php?isadd=true&total=`+total;  
+        }
+        
+        </script>
+
+          </head>
     
     <body>"
     
-    .$this->template.
-    
+    .$this->template.$this->alertSection().
     "<script src='$this->root/assets/bootstrap/js/bootstrap.min.js'></script>
     <script src='$this->root/assets/bootstrap/js/bootstrap.js'></script>
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/material-ui/5.0.0-beta.5/index.js' integrity='sha512-uKxirna7d5STmVXEMQYBVRW1nERrqHjwOubv4QcK4oYaaifLiEnN/aLIJxVsyK4R1K+awpNIG73RaQfT1DZ8ew==' crossorigin='anonymous' referrerpolicy='no-referrer'></script>
-    <script> function togo(url){ window.location.href = './Pages/productDetail.php?product='+url; } </script>
   
     </body>
 
@@ -62,22 +85,22 @@ class PageBody {
 
   // Methods
   function navBar() {
-    $this->template = $this->template.navigation($this->root);
+    $this->template = $this->template. navigation($this->root);
   }
 
   function slider() {
-    $this->template = $this->template.SliderCarousel();
+    $this->template = $this->template. SliderCarousel();
   }
 
   function banner() {
-    $this->template = $this->template.BannerSection($this->title);
+    $this->template = $this->template. BannerSection($this->title);
   }
   
   function aboutUs() {
     $query = $this->db_instant->querys->Getquery->getQuery_aboutUs;
     $result = $this->db_instant->getData($query);
 
-    $this->template = $this->template.aboutSection($this->root, $result);
+    $this->template = $this->template. aboutSection($this->root, $result);
   }
 
   function product(){    
@@ -85,59 +108,73 @@ class PageBody {
     $result = $this->db_instant->getData($query);
     
 
-    $this->template = $this->template.ProductSection($this->root,$result);
+    $this->template = $this->template. ProductSection($this->root,$result);
   }
 
   function categories() {    
     $query = $this->db_instant->querys->Getquery->getQuery_categories." LIMIT 0,3";
     $result = $this->db_instant->getData($query);
-    $this->template = $this->template.CategoriesSection($this->root,$result);
+
+    $this->template = $this->template. CategoriesSection($this->root,$result);
   }
 
   function newsLetter() {
-    $this->template = $this->template.newsLetterSection($this->root);
+    $this->template = $this->template. newsLetterSection($this->root);
   }
 
   function footer() {
-    $this->template = $this->template.FooterSection($this->root);
+    $this->template = $this->template. FooterSection($this->root);
   }
 
   function productHeader(){
-    $this->template = $this->template.ProductheaderSection($this->root,16);
+    $query = $this->db_instant->querys->Getquery->getQuery_Product." LIMIT 0,16";
+    $result = $this->db_instant->getData($query);
+
+    $this->template = $this->template. ProductheaderSection($this->root,$result);
   }
 
   function contactusSection(){
-    $this->template = $this->template.getContactUs();
+    $this->template = $this->template. getContactUs();
   }
 
   function loginSection(){
-    $this->template = $this->template.getLoginSection();
+    $this->template = $this->template. getLoginSection();
   }
   
   function signupSection(){
-    $this->template = $this->template.getSignUpSection();
+    $this->template = $this->template. getSignUpSection();
   }
 
-  function alertSection($message, $type){
-    $this->template = $this->template.getSnackBar($message, $type);
+  function alertSection(){
+    if(isset($_SESSION['Message']["message"])){
+      $type = isset($_SESSION['Message']["error"]) && !$_SESSION['Message']["error"]  ? "Success" : "Error";
+      $this->template = $this->template.getSnackBar($_SESSION['Message']["message"], $type);
+      unset($_SESSION['Message']);
+    }
   }
   
   function termSection(){
-    $this->template = $this->template.getTMC();
+    $this->template = $this->template. getTMC();
   }
 
   function detailsSection($productID){
-    $query = $this->db_instant->querys->Getquery->getQuery_Single_Product." where PID = '".$productID."'";
+
+    $query = $this->db_instant->querys->Getquery->getQuery_Single_Product." where `product`.PID = '".$productID."'";
     $result = $this->db_instant->getData($query);
-    $this->template = $this->template.getDetails($this->root, $result);
+    
+    $this->template = $this->template. getDetails($this->root, $result);
   }
 
-  function cartSection(){
-    $this->template = $this->template.getCart();
+  function cartSection(){  
+    $this->template = $this->template. getCart($this->root,$this->db_instant);
   }
 
   function checkOutSection(){
-    $this->template = $this->template.getCheckOut();
+    $this->template = $this->template. getCheckOut($this->root,$this->db_instant);
+  }
+  
+  function checkOutTotalSection(){
+    $this->template = $this->template. getcheckoutTotal();
   }
 }
 
