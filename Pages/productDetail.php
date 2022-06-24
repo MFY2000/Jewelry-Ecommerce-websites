35cfe63366556d5e5e0ec9975fa9c9fc1dcd8b46
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-06-24 11:18:36 +0500

    MFY auto commit at 24/06/2022 11:18:19

--- Pages/productDetail.php
+++ Pages/productDetail.php
@@ -3,12 +3,17 @@
     include '..\assets\php\MainBody.php'; 
 
     $index = new PageBody("product-detials", "..");
+    
+    if(!isset($_GET["product"])){
+        $_SESSION['Message']["message"] = "Unable to find specific product";
+        $_SESSION['Message']["error"] = true;
+        header("Location: ../index.php");
+    }
+    
     $index->navBar();
     $index->banner();
     $index->detailsSection($_GET["product"]);
     $index->footer();
     
     echo $index->body();
-    
-    $_SESSION["login"]["message"] = "";
     ?>
