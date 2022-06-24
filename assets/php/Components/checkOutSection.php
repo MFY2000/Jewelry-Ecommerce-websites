573eae4a6b7e3706972d8ff5a1e3bbb26472710d
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-06-24 11:18:38 +0500

    MFY auto commit at 24/06/2022 11:18:19

--- assets/php/Components/checkOutSection.php
+++ assets/php/Components/checkOutSection.php
@@ -1,163 +1,99 @@
 <?php
 
-  function getCheckOut()
-  {
-    return "
-    <!-- product start -->
-    <section class='cartPage'>
-        <div class='container'>
-            <div class='row'>
-                <div class='checkoutheader col-md-12'>
-                    <ul>
-                        <li>Item</li>
+
+    function getCartCheckout($root,$db_instant){
+        $checkouCart = "";
+        
+        foreach ($_SESSION['cart'] as $key => $value) {  
+            $query = $db_instant->querys->Getquery->Details_Cart. $key;
+            $result = $db_instant->getData($query);
+            
+            $data = $result->fetch_assoc();
+
+            $checkouCart = $checkouCart."<div class='checkOutRow row' id='r".$data['PID']."'>
+                <div class='col-md-6'>
+                    <div class='checkOutinfo'>
+                        <img src='$root/assets/images/Product/".$data['imageName']."' alt='".$data['Title']."' onload='setTimeout(() => {getCartQuantity(".$data['PID'].",".$value.")}, 2000);'  />
                         <div>
-                            <li>Quantity</li>
-                            <li>Price</li>
-                            <li>Total Price</li>
-                        </div>
-                    </ul>
-                </div>
-            </div>
-            <div class='row'>
-                <div class='col-md-12'>
-                    <div class='checkOutRow row'>
-                        <div class='col-md-6'>
-                            <div class='checkOutinfo'>
-                                <img src='./images/productPic1.png' alt='product pic 1' />
-                                <div>
-                                    <p class='primaryHeading'>Lorem ipsum</p>
-                                    <p class='secondaryHeading'>Lorem ipsum dolor sit amet,</p>
-                                    <p class='primaryParagraph'>Lorem ipsum dolor,</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class='checkOutprice col-md-6'>
-                            <div class='cartQuantity'>
-                                <a onclick='decrementer()'>-</a>
-                                <p id='quantityInc'>0</p>
-                                <a onclick='incrementer()'>+</a>
-                            </div>
-                            <div class='money'>
-                                <p>310 Rs</p>
-                                <p>350 Rs</p>
-                            </div>
-                            <div class='dfco'>
-                                <a href=''><i class='fa fa-trash'></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class='checkOutRow row'>
-                        <div class='col-md-6'>
-                            <div class='checkOutinfo'>
-                                <img src='./images/productPic4.png' alt='product pic 1' />
-                                <div>
-                                    <p class='primaryHeading'>Lorem ipsum</p>
-                                    <p class='secondaryHeading'>Lorem ipsum dolor sit amet,</p>
-                                    <p class='primaryParagraph'>Lorem ipsum dolor,</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class='checkOutprice col-md-6'>
-                            <div class='cartQuantity'>
-                                <a onclick='decrementer()'>-</a>
-                                <p id='quantityInc'>0</p>
-                                <a onclick='incrementer()'>+</a>
-                            </div>
-                            <div class='money'>
-                                <p>310 Rs</p>
-                                <p>350 Rs</p>
-                            </div>
-                            <div class='dfco'>
-                                <a href=''><i class='fa fa-trash'></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class='checkOutRow row'>
-                        <div class='col-md-6'>
-                            <div class='checkOutinfo'>
-                                <img src='./images/productPic4.png' alt='product pic 1' />
-                                <div>
-                                    <p class='primaryHeading'>Lorem ipsum</p>
-                                    <p class='secondaryHeading'>Lorem ipsum dolor sit amet,</p>
-                                    <p class='primaryParagraph'>Lorem ipsum dolor,</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class='checkOutprice col-md-6'>
-                            <div class='cartQuantity'>
-                                <a onclick='decrementer()'>-</a>
-                                <p id='quantityInc'>0</p>
-                                <a onclick='incrementer()'>+</a>
-                            </div>
-                            <div class='money'>
-                                <p>310 Rs</p>
-                                <p>350 Rs</p>
-                            </div>
-                            <div class='dfco'>
-                                <a href=''><i class='fa fa-trash'></i></a>
-                            </div>
+                            <p class='primaryHeading'>".$data['Title']."</p>
+                            <p class='secondaryHeading'>".substr($data['Details'], 0, 30)."</p>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class='row'>
-                <div class='col-md-12'>
-                    <div class='checkOutbottom row'>
-                        <div class='cobc1 col-md-6'>
-                            <h3>Checkout Summery</h3>
-                            <p class='primaryParagraph'>Lorem ipsum dolor sit amet,Lorem ipsumdolor sit amet Lorem ipsum dolor sit amet,Lorem ipsumdolor sit amet Lorem ipsum dolor sit amet,</p>
-                            <p class='copanh'>Have a copan code ? </p>
-                            <div class='contactfeilds'>
-                                <form>
-                                    <input class='contactInput' type='email' placeholder='Please enter your Copan Code'>
-                                    <button class='btnStyle'><i class='fa fa-arrow-right'></i></button>
-                                </form>
-                            </div>
-                        </div>
-                        <div class='col-md-6'>
-                            <div class='totalPrice'>
-                                <div class='thp row'>
-                                    <div class='col-md-6 col-sm-6 col-xs-6'>
-                                        <div class='th'>Sub total</div>
-                                        <div class='th'>Tax</div>
-                                    </div>
-                                    <div class='col-md-6 col-sm-6 col-xs-6'>
-                                        <div class='th2'>310 Rs</div>
-                                        <div class='th2'>50 Rs</div>
-                                    </div>
-                                </div>
-                                <div class='row'>
-                                    <div class='col-md-12'>
-                                        <div class='divider'></div>
-                                    </div>
-                                </div>
-                                <div class='thp row'>
-                                    <div class='col-md-6 col-sm-6 col-xs-6'>
-                                        <div class='th'>Total</div>
-                                    </div>
-                                    <div class='col-md-6 col-sm-6 col-xs-6'>
-                                        <div class='th2'>370 Rs</div>
-                                    </div>
-                                </div>
-                                <div class='chr row'>
-                                    <div class='col-md-12'>
-                                        <a href='' class='btnStyle'>Check Out</a>
-                                    </div>
-                                </div>
-                                <div class='chr row'>
-                                    <div class='chrh col-md-12 text-center'>
-                                        <a href='' class='chra'>Continue Shopping</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div class='checkOutprice col-md-6'>
+                    <div class='cartQuantity'>
+                        <a onclick='counterChnage(".$data['PID'].",true, 1)'>-</a>
+                        <p  id='Cart_".$data['PID']."'>$value</p>
+                        <a onclick='counterChnage(".$data['PID'].",false,".$data['Quantity'].")'>+</a>
+                    </div>
+                    <div class='money'>
+                        <p>$ <span id='Price_".$data['PID']."'>".$data['Price']."</span></p>
+                        <p>$ <span id='productTotal_".$data['PID']."'>".$data['Price']*$value."</span></p>
+                    </div>
+                    <div class='dfco'>
+                        <a ><i class='fa fa-trash'></i></a>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
-    <!-- product end -->
-";
+                </div>";
+        
+        }
+
+        return $checkouCart;
+    }
+
+    function getCheckOut($root,$db_instant){
+        return "<!-- product start -->
+
+    <script>
+       function getCartQuantity(id, quantity){
+        var oldQuantity = localStorage.getItem('Cart_'+id);
+        
+        if(quantity != oldQuantity){
+            var difference = (oldQuantity-quantity);
+            
+            if(difference > 0){
+                document.getElementById('Cart_'+id).innerHTML = oldQuantity;
+            }
+
+            
+            setTimeout(() => {
+                var price = parseInt(document.getElementById('Price_'+id).innerHTML);
+                var CartTotal = document.getElementById('CartTotal');
+                var ProductTotal = document.getElementById('productTotal_'+id);
+                ProductTotal.innerHTML = parseInt(ProductTotal.innerHTML)+(price * difference);
+                CheckoutTotal();
+            }, 1000); 
+        }
+    }
+    
+        function onclickableHref(id,price){
+            var quantity = document.getElementById('productTotal_'+id);
+            window.location.href = '$root/assets/php/Services/Form/CheckoutFunction.php?isadd=false&id='+id+'&p1='+price+'&p2='+quantity;
+        }
+
+    </script>
+        <section class='cartPage'>
+            <div class='container'>
+                <div class='row'>
+                    <div class='checkoutheader col-md-12'>
+                        <ul>
+                            <li>Item</li>
+                            <div>
+                                <li>Quantity</li>
+                                <li>Price</li>
+                                <li>Total Price</li>
+                                <li>Delete</li>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+                <div class='row'>
+                    <div class='col-md-12'>
+                        ".getCartCheckout($root,$db_instant)."
+                    </div>
+                </div>
+                
+        <!-- product end -->";
   }
 
 ?>
