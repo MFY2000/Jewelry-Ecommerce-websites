a0271d9c809a8faf719328084b956574847a7402
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-06-26 12:45:59 +0500

    MFY auto commit at 26/06/2022 12:21:39

--- assets/php/Components/CheckoutTotalSection.php
+++ assets/php/Components/CheckoutTotalSection.php
@@ -1,6 +1,14 @@
 <?php
-  function getcheckoutTotal(){
-    return "<div class='row'>
+  function getcheckoutTotal($root){
+    return "
+    <script>
+    
+        function getCartQuantity(id, quantity){
+
+       }
+    
+       </script>
+    <div class='row'>
     <div class='col-md-12'>
         <div class='checkOutbottom row'>
             <div class='cobc1 col-md-6'>
@@ -9,8 +17,8 @@
                 <p class='copanh'>Have a copan code ? </p>
                 <div class='contactfeilds'>
                     <form>
-                        <input class='contactInput' type='email' placeholder='Please enter your Copan Code'>
-                        <button class='btnStyle'><i class='fa fa-arrow-right'></i></button>
+                        <input class='contactInput' type='text' name='CopanCode' placeholder='Please enter your Copan Code'>
+                        <button class='btnStyle' type='submit'><i class='fa fa-arrow-right'></i></button>
                     </form>
                 </div>
             </div>
@@ -23,9 +31,9 @@
                             <div class='th'>Tax</div>
                         </div>
                         <div class='col-md-6 col-sm-6 col-xs-6'>
-                            <div class='th2'>$ <p id='Subtotal'>".$_SESSION['checkOut']["total"]."</p></div>
-                            <div class='th2'>$ <p id='Discount'>0</p></div>
-                            <div class='th2'>$ <p id='Taxtotal'>50</p></div>
+                            <span class='th2'>$ <p id='Subtotal'>".$_SESSION['checkOut']["total"]."</p></span>
+                            <span class='th2'>$ <p id='Discount'>0</p></span>
+                            <span class='th2'>$ <p id='Taxtotal'>50</p></span>
                         </div>
                     </div>
                     <div class='row'>
@@ -38,17 +46,17 @@
                             <div class='th'>Total</div>
                         </div>
                         <div class='col-md-6 col-sm-6 col-xs-6'>
-                            <div class='th2'>370 Rs</div>
+                            <span class='th2'>$ <p id='total'>0</p></span>
                         </div>
                     </div>
                     <div class='chr row'>
                         <div class='col-md-12'>
-                            <a href='' class='btnStyle'>Check Out</a>
+                            <a href='".$root."/assets/php/Services/Form/BookOrder.php?data=".json_encode($_SESSION['cart'])."&total' class='btnStyle'>Check Out</a>
                         </div>
                     </div>
                     <div class='chr row'>
                         <div class='chrh col-md-12 text-center'>
-                            <a href='' class='chra'>Continue Shopping</a>
+                            <a href='$root/index.php' class='chra'>Continue Shopping</a>
                         </div>
                     </div>
                 </div>
