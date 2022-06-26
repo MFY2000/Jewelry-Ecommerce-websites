3fde52e580224f4de72906cbc5b85d913b0db731
Muhammad Fahad <fa19bsse0014@maju.edu.pk>
2022-06-26 12:45:59 +0500

    MFY auto commit at 26/06/2022 12:21:39

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,35 @@
-function togo(url){ window.location.href = '$this->root/Pages/productDetail.php?product='+url; } 
-        
-        function counter(id,value,condition,limit){ 
-          var element = document.getElementById(id); 
-          var actionDone = false;
-    
-          if(condition && element.innerHTML > limit){ 
-            element.innerHTML = -(-element.innerHTML - value);
-            actionDone = true;
-          } 
-          else if(element.innerHTML < limit){ 
-            element.innerHTML = -(-element.innerHTML - value);
-            actionDone = true;
-          } 
-    
-          addToCart(id, element.innerHTML);
-          return actionDone;
-        } 
-    
-        function addToCart(id, quantity){
-          localStorage.setItem(id, quantity);
-        }
-    
-    
-        function counterChnage(id,condition,limit){ 
-          var change = (condition ? -1 : 1);
-          var price = parseInt(document.getElementById('Price_'+id).innerHTML);
-          var ProductTotal = document.getElementById('productTotal_'+id);
-          var CartTotal = document.getElementById('CartTotal');
-    
-          actionDone = counter(('Cart_'+id), change,condition,limit);
-      
-          if(actionDone){
-              ProductTotal.innerHTML = parseInt(ProductTotal.innerHTML)+(price * change);
-              CartTotal.innerHTML = parseInt(CartTotal.innerHTML)+(price * change);
-          }
-        }
-      
-        
+function counter(id, value, condition, limit) {
+  var element = document.getElementById(id);
+  var actionDone = false;
+
+  if (condition && element.innerHTML > limit) {
+    element.innerHTML = -(-element.innerHTML - value);
+    actionDone = true;
+  } else if (element.innerHTML < limit) {
+    element.innerHTML = -(-element.innerHTML - value);
+    actionDone = true;
+  }
+
+  addToCart(id, element.innerHTML);
+  return actionDone;
+}
+
+function addToCart(id, quantity) {
+  localStorage.setItem(id, quantity);
+}
+
+function counterChnage(id, condition, limit) {
+  var change = condition ? -1 : 1;
+  var price = parseInt(document.getElementById("Price_" + id).innerHTML);
+  var ProductTotal = document.getElementById("productTotal_" + id);
+  var CartTotal = document.getElementById("CartTotal");
+
+  actionDone = counter("Cart_" + id, change, condition, limit);
+
+  if (actionDone) {
+    ProductTotal.innerHTML = parseInt(ProductTotal.innerHTML) + price * change;
+    CartTotal.innerHTML = parseInt(CartTotal.innerHTML) + price * change;
+  }
+}
+
+
